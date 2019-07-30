0601c91ed6582b642d49d6b626313bdcc55877fc
Dmitry Chukedov <d4yk@yandex.ru>
2019-07-31 01:38:33 +0300

    add product tests

--- pages/locators.py
+++ pages/locators.py
@@ -8,4 +8,13 @@ class MainPageLocators(object):
 class LoginPageLocators(object):
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators(object):
+    ADD_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRICE = (By.CSS_SELECTOR, ".price_color")     
+    PRODUCT = (By.CSS_SELECTOR, "div.col-sm-6:nth-child(2) > h1:nth-child(1)")
+    PRICE_ALLERT = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+    PRODUCT_ALLERT = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")       
+
     
---
+++ pages/product_page.py
@@ -0,0 +1,31 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def should_be_product_page(self):
+        self.should_be_login_url()
+        self.should_add_to_cart()
+        self.should_be_register_form()
+        self.add_to_cart()
+        self.should_be_correct_product()
+        self.should_be_correct_price()
+        
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url , "Login is not in URL"
+
+    def should_add_to_cart(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_BUTTON), "Add to cart button is not presented"
+
+    def add_to_cart(self):
+        btn = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        btn.click()
+
+    def should_be_correct_product(self):
+        assert self.browser.find_element(*ProductPageLocators.PRODUCT).text in self.browser.find_element(*ProductPageLocators.PRODUCT_ALLERT).text, "Product is not correct"
+
+    def should_be_correct_price(self):
+        assert self.browser.find_element(*ProductPageLocators.PRICE).text in self.browser.find_element(*ProductPageLocators.PRICE_ALLERT).text, "Price is not correct"
+        
